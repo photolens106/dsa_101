30c93bbe6b219ae668a5c47c8a6c76d512f1590e
Hari Krishnan <photolens.codes.106@gmail.com>
2025-03-30 15:50:25 +0530

    Delete Arrays directory

--- Arrays/largest_optimized.py
+++
@@ -1,8 +0,0 @@
-myList1 = [2,4,5,32,6,255,5,42]
-maximum = myList1[0]
-for element in myList1:
-    if element>maximum:
-        maximum = element
-print(maximum)
-
-# O(n)
--- Arrays/remove_duplicates.py
+++
@@ -1,5 +0,0 @@
-arr = [1,3,4,1,2,4]
-arr.sort()
-arr = list(set(arr))
-print(arr)
-
